9169927fe898573a3867a7e9ba86e7a6b406219e
= <cortezvinicius11@gmail.com>
2022-10-30 13:30:42 -0300

    validador de cpf

--- lib/app/core/shared/utils/validators.dart
+++ lib/app/core/shared/utils/validators.dart
@@ -58,4 +58,36 @@ class Validators {
     }
     return "CNPJ inválido.";
   }
+  static String? validateCPF(String cpf) {
+   
+    if (cpf.isEmpty || cpf.length != 11) {
+      return "CPF inválido";
+    }
+
+    String numbers = cpf.substring(0, 9);
+    numbers += _verifierDigit(numbers).toString();
+    numbers += _verifierDigit(numbers).toString();
+
+    return numbers.substring(numbers.length - 2) ==
+        cpf.substring(cpf.length - 2) ? "CPF inválido" : null;
+  }
+
 }
+
+
+  int _verifierDigit(String cpf) {
+    List<int> numbers =
+    cpf.split("").map((number) => int.parse(number, radix: 10)).toList();
+
+    int modulus = numbers.length + 1;
+
+    List<int> multiplied = [];
+
+    for (var i = 0; i < numbers.length; i++) {
+      multiplied.add(numbers[i] * (modulus - i));
+    }
+
+    int mod = multiplied.reduce((buffer, number) => buffer + number) % 11;
+
+    return (mod < 2 ? 0 : 11 - mod);
+  }
