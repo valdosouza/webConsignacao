1cc832748537beff193fef205088b5a3c4877776
leonardomaia <leuow95@gmail.com>
2022-11-01 23:57:19 -0400

    #38 feat: implemented sales repositoru

---
+++ lib/app/modules/sales_register/data/repository/sales_register_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:appweb/app/core/error/exceptions.dart';
+import 'package:appweb/app/modules/sales_register/data/datasource/remote/ownapi/sales_data_source.dart';
+import 'package:appweb/app/modules/sales_register/data/models/sales_model.dart';
+import 'package:appweb/app/core/error/failures.dart';
+import 'package:appweb/app/modules/sales_register/domain/repository/sales_register_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class SalesRepositoryImpl implements SalesRepository {
+  final SalesDataSource datasource;
+
+  SalesRepositoryImpl({
+    required this.datasource,
+  });
+
+  @override
+  Future<Either<Failure, SalesModel>> post({
+    required SalesModel model,
+  }) async {
+    try {
+      final result = await datasource.post(model: model);
+      return Right(result);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+}
