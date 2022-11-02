bf1a9d649ac0f04deae2552db3390d6faf13bc87
leonardomaia <leuow95@gmail.com>
2022-11-02 00:52:13 -0400

    #38 feat: implemented sales bloc states

---
+++ lib/app/modules/sales_register/presentation/bloc/sales_states.dart
@@ -0,0 +1,27 @@
+import '../../data/models/sales_model.dart';
+
+abstract class SalesState {
+  final List<SalesModel> listSalesModel;
+
+  SalesState({
+    required this.listSalesModel,
+  });
+}
+
+class SalesSuccessSate extends SalesState {
+  SalesSuccessSate({
+    required super.listSalesModel,
+  });
+}
+
+class SalesAddSuccessSate extends SalesState {
+  SalesAddSuccessSate({
+    required super.listSalesModel,
+  });
+}
+
+class SalesAddErrorSate extends SalesState {
+  SalesAddErrorSate({
+    required super.listSalesModel,
+  });
+}
