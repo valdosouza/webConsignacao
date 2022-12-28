2c0c14c97f32ddf08b73443218289ff86a0eafe3
= <cortezvinicius11@gmail.com>
2022-12-28 16:29:20 -0300

    fix mobile

--- lib/app/modules/stock_balance_customer/presentation/pages/stock_balance_customer_page_mobile.dart
+++ lib/app/modules/stock_balance_customer/presentation/pages/stock_balance_customer_page_mobile.dart
@@ -1,6 +1,12 @@
 import 'package:appweb/app/core/shared/theme.dart';
-import 'package:appweb/app/modules/stock/stock_module.dart';
+import 'package:appweb/app/modules/Core/data/model/stock_balance_model.dart';
+import 'package:appweb/app/modules/stock_balance_customer/presentation/bloc/stock_balance_customer_bloc.dart';
+import 'package:appweb/app/modules/stock_balance_customer/presentation/bloc/stock_balance_customer_event.dart';
+import 'package:appweb/app/modules/stock_balance_customer/presentation/bloc/stock_balance_customer_state.dart';
+import 'package:appweb/app/modules/stock_balance_customer/presentation/contents/content_stock_balance_customer.dart';
+import 'package:appweb/app/modules/stock_balance_customer/stock_balance_customer_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class StockBalanceCustomerPageMobile extends StatefulWidget {
@@ -13,22 +19,55 @@ class StockBalanceCustomerPageMobile extends StatefulWidget {
 
 class StockBalanceCustomerPageMobileState
     extends State<StockBalanceCustomerPageMobile> {
+  late final StockBalanceCustomerBloc bloc;
+
   @override
   void initState() {
     super.initState();
     Future.delayed(const Duration(milliseconds: 100)).then((_) async {
-      await Modular.isModuleReady<StockModule>();
+      await Modular.isModuleReady<StockBalanceCustomerModule>();
     });
+    bloc = Modular.get<StockBalanceCustomerBloc>();
+    bloc.add(StockBalanceCustomerGetListEvent(tbInstitutionId: 1));
   }
 
   @override
   Widget build(BuildContext context) {
+    return BlocConsumer<StockBalanceCustomerBloc, StockBalanceCustomerState>(
+      bloc: bloc,
+      listener: (context, state) {
+        statesStockBalance(state);
+      },
+      builder: (context, state) {
+        if (state is StockBalanceCustomerLoadingState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final stockBalance = state.list;
+        return _listastockBalance(stockBalance);
+      },
+    );
+  }
+
+  _listastockBalance(List<StockBalanceModel> customers) {
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
           decoration: kBoxDecorationflexibleSpace,
         ),
-        title: const Text("Balanço de estoque geral"),
+        title: const Text('Lista de Balanço de Estoque Cliente'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(children: [
+            buildSearchInput(bloc),
+            const SizedBox(height: 15),
+            buildListView(bloc.stockBalance),
+          ]),
+        ),
       ),
     );
   }
--- lib/app/modules/stock_balance_general/presentation/pages/stock_balance_general_page_mobile.dart
+++ lib/app/modules/stock_balance_general/presentation/pages/stock_balance_general_page_mobile.dart
@@ -1,6 +1,12 @@
 import 'package:appweb/app/core/shared/theme.dart';
-import 'package:appweb/app/modules/stock/stock_module.dart';
+import 'package:appweb/app/modules/Core/data/model/stock_balance_model.dart';
+import 'package:appweb/app/modules/stock_balance_general/presentation/bloc/stock_balance_general_bloc.dart';
+import 'package:appweb/app/modules/stock_balance_general/presentation/bloc/stock_balance_general_event.dart';
+import 'package:appweb/app/modules/stock_balance_general/presentation/bloc/stock_balance_general_state.dart';
+import 'package:appweb/app/modules/stock_balance_general/presentation/contents/content_stock_balance_general.dart';
+import 'package:appweb/app/modules/stock_balance_general/stock_balance_general_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class StockBalanceGeneralPageMobile extends StatefulWidget {
@@ -13,22 +19,57 @@ class StockBalanceGeneralPageMobile extends StatefulWidget {
 
 class StockBalanceGeneralPageMobileState
     extends State<StockBalanceGeneralPageMobile> {
+  late final StockBalanceGeneralBloc bloc;
+
   @override
   void initState() {
     super.initState();
     Future.delayed(const Duration(milliseconds: 100)).then((_) async {
-      await Modular.isModuleReady<StockModule>();
+      await Modular.isModuleReady<StockBalanceGeneralModule>();
     });
+    bloc = Modular.get<StockBalanceGeneralBloc>();
+    bloc.add(StockBalanceGeneralGetListEvent(tbInstitutionId: 1));
   }
 
   @override
   Widget build(BuildContext context) {
+    return BlocConsumer<StockBalanceGeneralBloc, StockBalanceGeneralState>(
+      bloc: bloc,
+      listener: (context, state) {
+        statesStockBalance(state);
+      },
+      builder: (context, state) {
+        if (state is StockBalanceGeneralLoadingState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final stockBalance = state.list;
+        return _listastockBalance(stockBalance);
+      },
+    );
+  }
+
+  _listastockBalance(List<StockBalanceModel> customers) {
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
           decoration: kBoxDecorationflexibleSpace,
         ),
-        title: const Text("Balanço de estoque geral"),
+        title: const Text('Lista de Balanço de Estoque Geral'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              buildSearchInput(bloc),
+              const SizedBox(height: 15),
+              buildListView(bloc.stockBalance),
+            ],
+          ),
+        ),
       ),
     );
   }
